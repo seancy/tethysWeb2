d393f0972d16366810fd92085978763cdf6cf4a2
Double <ginbei.design@gmail.com>
2017-09-30 16:02:02 +0800

    彈窗、首頁修改

--- static/js/ui.js
+++ static/js/ui.js
@@ -28,7 +28,38 @@ $(function(){
         $(this).parent().stop().animate({right:'-130px'});
     })
 })
+// 弹窗
 
+$(function(){
+	var mdwBtn = $('.modalBtn'),
+	overlayOpacity = 0.7,
+	fadeTime = 300;
 
+	mdwBtn.on('click',function(e){
+		e.preventDefault();
+
+		var setMdw = $(this),
+		setHref = setMdw.attr('href'),
+		setSource = $(setHref).html(),
+		wdHeight = $(window).height();
+
+		$('body').append('<div id="mdOverlay"></div><div id="mdWindow"><div class="mdClose icon_sprite icon_close"></div><div class="modalContent">' + setSource + '</div></div>');
+
+		$('#mdOverlay,#mdWindow').css({display:'block',opacity:'0'});
+		$('#mdOverlay').css({height:wdHeight}).stop().animate({opacity:overlayOpacity},fadeTime);
+		$('#mdWindow').stop().animate({opacity:'1'},fadeTime);
+
+		$(window).on('resize',function(){
+			var adjHeight = $(window).height();
+			$('#mdOverlay').css({height:adjHeight});
+		});
+
+		$('#mdOverlay,.mdClose,.btn_modal').on('click',function(){
+			$('#mdWindow,#mdOverlay').stop().animate({opacity:'0'},fadeTime,function(){
+				$('#mdOverlay,#mdWindow').remove();
+			});
+		});
+	});
+});
 
 
