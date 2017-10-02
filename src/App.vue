d0959e9f6d98c00c60b876a2b9e0de48a112fcb8
Sean <119115@gmail.com>
2017-10-02 17:49:34 +0800

    defeloped of module of index

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div>
+    <topHeader></topHeader>
+    <!-- <div id="app"> -->
+      <!-- <img src="./assets/logo.png"> -->
+      <router-view></router-view>
+    <!-- </div> -->
+    <bottomFooter></bottomFooter>
+    <onlineService></onlineService>
+    <!-- <popupAd></popupAd> -->
   </div>
 </template>
 
 <script>
+import '../static/css/main.css'
+import '../static/css/pages.css'
+import TopHeader from '@/components/TopHeader'
+import BottomFooter from '@/components/BottomFooter'
+import OnlineService from '@/components/OnlineService'
+import PopupAd from '@/components/PopupAd'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TopHeader,
+    BottomFooter,
+    OnlineService,
+    PopupAd
+  }
+  // template: '<div><topHeader></topHeader>test info.</div>'
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
---
+++ src/components/BottomFooter.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div id="footer">
+            <div class="contain_width">
+                <div class="link_list">
+                    <dl class="link_block parner">
+                        <dt>合作伙伴</dt>
+                        <dd>
+                            <a class="link-ag"></a>
+                            <a class="link-mg"></a>
+                            <a class="link-bb"></a>
+                            <a class="link-playtech"></a>
+                            <a class="link-lottery"></a>
+                            <a class="link-oriental"></a>
+                            <a class="link-allbet"></a>
+                        </dd>
+                    </dl>
+                    <dl class="link_block">
+                        <dt>牌照</dt>
+                        <dd>
+                            <a class="link-pagcor"></a>
+                        </dd>
+                    </dl>
+                </div>
+                <div class="link_list">
+                    <dl class="link_block pay">
+                        <dt>支付方式</dt>
+                        <dd>
+                            <a class="link-visa"></a>
+                            <a class="link-mastercard"></a>
+                            <a class="link-unionpay"></a>
+                            <a class="link-wechatpay"></a>
+                            <a class="link-alipay"></a>
+                        </dd>
+                    </dl>
+                    <dl class="link_block responsibility">
+                        <dt>责任博彩</dt>
+                        <dd>
+                            <a class="link-18"></a>
+                            <a class="link-gavcare"></a>
+                            <a class="link-hand"></a>
+                        </dd>
+                    </dl>
+                </div>
+                <div class="footer_menu">
+                    <ul>
+                        <li><a href="about.html">关于我们</a></li>
+                        <li><a href="policy.html">隐私政策</a></li>
+                        <li><a href="duty.html">责任博彩</a></li>
+                        <li><a href="disclaimer.html">免责声明</a></li>
+                        <li><a href="join.html">代理加盟</a></li>
+                        <li><a href="contact.html">联系我们</a></li>
+                        <li><a href="help.html">帮助中心</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">Cparisian Casino © 版权所有 2011-2017属于Las Vegas Sands拉斯维加斯金沙集团(中国)有限公司注册运营</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'bottomFooter',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
