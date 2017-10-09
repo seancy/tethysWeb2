b16e78e1cc8f8652bab7138e5d4c355301efe3f0
Double <ginbei.design@gmail.com>
2017-10-09 14:34:14 +0800

    體育競技樣式調整

--- src/components/Sports.vue
+++ src/components/Sports.vue
@@ -138,29 +138,6 @@ export default {
 }
 </script>
 <style scoped>
-  .sports_list{ width: 1020px; margin: 40px auto;}
-  .sports_list .sports_list_content{ margin:0px; padding: 0px;}
-  .sports_list .sports_list_content li{ list-style: none; padding:0px; width: 48%; height: 374px; display: inline-table; margin: 40px 1% 30px; position: relative; text-align: center; border: 2px solid #313131; float: left;}
-  .sports_list .sports_list_content li:hover{border-color: #ff6000;box-shadow: 0px 0px 30px rgba(0,0,0,0.6);-webkit-transition: border-color 300ms ease-in;-moz-transition: border-color 300ms ease-in;-ms-transition: border-color 300ms ease-in;transition: border-color 300ms ease-in; }
-  .sports_list .sports_list_content li .bg {width: 485px; height: 317px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: -40px -370px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 500ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;}
-  .sports_list .sports_list_content li:hover .bg{background-position-x: 0px;}
-  .sports_list .sports_list_content li .img {width: 485px;height: 370px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: 40px 0px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 200ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;}
-  .sports_list .sports_list_content li:hover .img{background-position-x: -20px;}
-  .sports_list .sports_list_content li .title{height: 108px; position: absolute;bottom: 0;left: 0;z-index: 3;width: 100%;padding: 15px;text-align: center;background-color: rgba(0,0,0,0.8);}
-  .sports_list .sports_list_content li .title_img{margin-top: 5px;width: 150px;height: 50px;background-size: 100%;display: inline-block;float: left;}
-  .sports_list .sports_list_content li.sport_ug .title_img{background-image: url(../../static/css/img/logo_ug.png); width: 208px; height: 76px;}
-  .sports_list .sports_list_content li.sport_bb .title_img{background-image: url(../../static/css/img/logo_bb.png); width:126px; height: 68px;}
-  .sports_list .sports_list_content li h2{font-size: 18px;margin-top: 23px; display: inline-block;margin-left: 10px; color: #fff;font-weight: normal;vertical-align: bottom; float: left;}
-  .sports_list .sports_list_content li .btn_enter{position: absolute;bottom: 13px;z-index: 4;left: 50%;}
-  .sports_list .sports_list_content li .gameopen_parent{margin-left: 100px; display: inline-block;margin-top: 10px;color: #fff;line-height: 34px;height: 34px;padding: 0px 30px;background-color: #ff6000;border-radius: 17px;}
-  .pagebox .pagemenu_slider {
 
-    text-align: center;
-  }
-  .pagebox .item_pagemenu {
-
-    width: 192px;
-    display: inline-block;
-  }
 
 </style>
