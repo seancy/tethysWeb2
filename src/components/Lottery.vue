cb45b81461fd3dc5ee32666d194522243f2b4c44
webb <bc001122@outlook.com>
2017-10-09 11:31:52 +0800

    体育 彩票页面

--- src/components/Lottery.vue
+++ src/components/Lottery.vue
@@ -3,12 +3,12 @@
       <div class="pagebox" id="lottery">
           <div class="contain_width">
               <h2 class="page_title"></h2>
-              <!--品牌选单-->
-              <div class="pagemenu_slider">
+              <!--品牌选单 先不用-->
+            <!--  <div class="pagemenu_slider">
                   <div class="item_pagemenu" v-for="list in games">
-                      <a href="javascript:;" data-list="lottery_bb"  @click="enterGame(list.id)"> <!-- class="active" -->
+                      <a href="javascript:;" data-list="lottery_bb"  @click="enterGame(list.id)"> &lt;!&ndash; class="active" &ndash;&gt;
                           <div class="img">
-                             <!-- <span style="background-image: url(../../static/images/brand/bbin.png)"></span>-->
+                             &lt;!&ndash; <span style="background-image: url(../../static/images/brand/bbin.png)"></span>&ndash;&gt;
                               <span  :style="{backgroundImage: 'url('+[photo_url+'/pic/'+list.img+'/0']+')'}"></span>
                           </div>
                           <h3>BB彩票</h3>
@@ -21,7 +21,35 @@
                       </a>
                   </div>
 
+              </div>-->
+
+              <!--彩票遊戲-->
+              <div class="lotterys_list">
+                  <ul class="lotterys_list_content">
+                      <li class="lottery_bb" v-for="list in games">
+                          <div class="img_div">
+                              <div class="img"  :style="{backgroundImage: 'url('+[photo_url +'/pic/'+[list.img]+'/0']+')'}"></div>
+                              <div class="title">
+                                  <div class="title_img"></div>
+                                  <div class="btn_enter"> <a class="gameopen_parent" href="javascript:;" @click="enterGame(list.id)">进入游戏</a> </div>
+                              </div>
+                          </div>
+                      </li>
+                      <li class="lottery_bc">
+                          <div class="img_div">
+                              <div class="img" style="background-image:url(/static/images/lo_bc.png)"></div>
+                              <!-- <div class="title">
+                                <div class="title_img"></div>
+                                <div class="btn_enter"> <a class="gameopen_parent" href="javascript:;">进入游戏</a> </div>
+                              </div> -->
+                          </div>
+                      </li>
+
+
+                  </ul>
               </div>
+              <!--end 彩票遊戲-->
+
           </div>
       </div>
 
@@ -35,7 +63,8 @@ export default {
     return {
       hasLogin: false,
       games: [],
-      game_name:{"862":"BB彩票"} ,
+     // game_name:{"862":"BB彩票"} ,
+      game_sub_image:{"":"lottery_bb","":"lottery_bc"}
     }
   }, 
   created:function(){
@@ -103,5 +132,20 @@ export default {
         width: 192px;
         display: inline-block;
     }
+    .lotterys_list{ width: 1020px; margin: 0 auto;}
+    .lotterys_list .lotterys_list_content{ margin:0px; padding: 0px;}
+    .lotterys_list .lotterys_list_content li{ list-style: none; padding:0px; width: 48%; height: 250px; display: inline-table; margin: 0px 1% 30px; position: relative; text-align: center; border: 2px solid #313131; float: left;}
+    .lotterys_list .lotterys_list_content li:hover{border-color: #ff6000;box-shadow: 0px 0px 30px rgba(0,0,0,0.6);-webkit-transition: border-color 300ms ease-in;-moz-transition: border-color 300ms ease-in;-ms-transition: border-color 300ms ease-in;transition: border-color 300ms ease-in; }
+    .lotterys_list .lotterys_list_content li .bg {width: 485px; height:323px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: -40px -363px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 500ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;background-size: 110%;}
+    .lotterys_list .lotterys_list_content li:hover .bg{background-position-x: 0px;}
+    .lotterys_list .lotterys_list_content li .img {width: 485px;height: 246px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: 0  px 0px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 200ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;background-size: 110%;}
+    .lotterys_list .lotterys_list_content li:hover .img{background-position-x: -20px;}
+    .lotterys_list .lotterys_list_content li .title{height: 108px; position: absolute;bottom: 0;left: 0;z-index: 3;width: 100%;padding: 15px;text-align: center;background-color: rgba(0,0,0,0.8);}
+    .lotterys_list .lotterys_list_content li .title_img{margin-top: 5px;width: 150px;height: 50px;background-size: 100%;display: inline-block;float: left;}
+    .lotterys_list .lotterys_list_content li.lottery_bc .title_img{    background-image: url(../../static/images/logo_bc.png);width: 208px;height: 62px;}
+    .lotterys_list .lotterys_list_content li.lottery_bb .title_img{background-image: url(../../static/images/logo_bb.png); width:126px; height: 68px;}
+    .lotterys_list .lotterys_list_content li h2{font-size: 18px;margin-top: 23px; display: inline-block;margin-left: 10px; color: #fff;font-weight: normal;vertical-align: bottom; float: left;}
+    .lotterys_list .lotterys_list_content li .btn_enter{position: absolute;bottom: 13px;z-index: 4;left: 50%;}
+    .lotterys_list .lotterys_list_content li .gameopen_parent{margin-left: 100px; display: inline-block;margin-top: 10px;color: #fff;line-height: 34px;height: 34px;padding: 0px 30px;background-color: #ff6000;border-radius: 17px;}
 
 </style>
--- src/components/Sports.vue
+++ src/components/Sports.vue
@@ -3,25 +3,49 @@
     <div class="pagebox" id="lottery">
       <div class="contain_width">
         <h2 class="page_title"></h2>
-        <!--品牌选单-->
-        <div class="pagemenu_slider" >
+        <!--品牌选单 先不用-->
+       <!-- <div class="pagemenu_slider" >
           <div class="item_pagemenu" v-for="list in computers">
             <a href="javascript:;" @click="enterGame(list.id)" >
               <div class="img">
-               <!-- <span style="background-image: url(../../static/images/brand/bbin.png)"></span>-->
+               &lt;!&ndash; <span style="background-image: url(../../static/images/brand/bbin.png)"></span>&ndash;&gt;
                 <span  :style="{backgroundImage: 'url('+[photo_url+'/pic/'+list.img+'/0']+')'}" ></span>
               </div>
               <h3>{{game_name[list.id]}}</h3>
             </a>
           </div>
-       <!--   <div class="item_pagemenu">
+       &lt;!&ndash;   <div class="item_pagemenu">
             <a href="javascript:;"  @click="enterGame(computers[1].id)">
               <div class="img"><span style="background-image: url(../../static/images/brand/ug.png)"></span></div>
               <h3>{{computers[1].name}}</h3>
             </a>
-          </div>-->
-        </div>
+          </div>&ndash;&gt;
+        </div>-->
+        <div class="sports_list">
+          <ul class="sports_list_content">
+            <li :class="game_sub_image[item.id]" v-for="(item, index) in computers">
+              <div class="img_div">
+                <div class="bg" :style="{backgroundImage: 'url('+[photo_url +'/pic/'+[item.img]+'/0']+')'}"></div>
+                <div class="img" :style="{backgroundImage: 'url('+[photo_url +'/pic/'+[item.img]+'/0']+')'}"></div>
+                <div class="title">
+                  <div class="title_img"></div>
+                  <div class="btn_enter">
+                    <a class="gameopen_parent" href="javascript:;" @click="enterGame(item.id)">进入游戏</a>
+                  </div>
+                </div>
+              </div>
+            </li>
 
+          </ul>
+          <!--
+              <a class="carousel-prev" href="#carousel-tag" data-slide="prev">
+                  <i class="fa fa-angle-left" aria-hidden="true"></i>
+              </a>
+              <a class="carousel-next" href="#carousel-tag" data-slide="next">
+                  <i class="fa fa-angle-right" aria-hidden="true"></i>
+              </a>
+          -->
+        </div>
       </div>
 
       <div class="contain_width">
@@ -114,24 +138,21 @@ export default {
 }
 </script>
 <style scoped>
-  /*.sports-show { padding-top: 105px; background: #1F1F1F center center no-repeat; }*/
-  /*.banner-sports { height: 295px; background: url(../../static/css/img/banner-sports.png) top center no-repeat; position: relative; background-size: 1920px auto; }*/
-
-  /*.sports_list{ width: 1020px; margin: 40px auto;}*/
-  /*.sports_list .sports_list_content{ margin:0px; padding: 0px;}*/
-  /*.sports_list .sports_list_content li{ list-style: none; padding:0px; width: 48%; height: 374px; display: inline-table; margin: 40px 1% 30px; position: relative; text-align: center; border: 2px solid #313131; float: left;}*/
-  /*.sports_list .sports_list_content li:hover{border-color: #ff6000;box-shadow: 0px 0px 30px rgba(0,0,0,0.6);-webkit-transition: border-color 300ms ease-in;-moz-transition: border-color 300ms ease-in;-ms-transition: border-color 300ms ease-in;transition: border-color 300ms ease-in; }*/
-  /*.sports_list .sports_list_content li .bg {width: 485px; height: 405px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: -40px -370px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 500ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;}*/
-  /*.sports_list .sports_list_content li:hover .bg{background-position-x: 0px;}*/
-  /*.sports_list .sports_list_content li .img {width: 485px;height: 370px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: 40px 0px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 200ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;}*/
-  /*.sports_list .sports_list_content li:hover .img{background-position-x: -20px;}*/
-  /*.sports_list .sports_list_content li .title{height: 108px; position: absolute;bottom: 0;left: 0;z-index: 3;width: 100%;padding: 15px;text-align: center;background-color: rgba(0,0,0,0.8);}*/
-  /*.sports_list .sports_list_content li .title_img{margin-top: 5px;width: 150px;height: 50px;background-size: 100%;display: inline-block;float: left;}*/
-  /*.sports_list .sports_list_content li.sport_ug .title_img{background-image: url(../../static/css/img/logo_ug.png); width: 208px; height: 76px;}*/
-  /*.sports_list .sports_list_content li.sport_bb .title_img{background-image: url(../../static/css/img/logo_bb.png); width:126px; height: 68px;}*/
-  /*.sports_list .sports_list_content li h2{font-size: 18px;margin-top: 23px; display: inline-block;margin-left: 10px; color: #fff;font-weight: normal;vertical-align: bottom; float: left;}*/
-  /*.sports_list .sports_list_content li .btn_enter{position: absolute;bottom: 13px;z-index: 4;left: 50%;}*/
-  /*.sports_list .sports_list_content li .gameopen_parent{margin-left: 100px; display: inline-block;margin-top: 10px;color: #fff;line-height: 34px;height: 34px;padding: 0px 30px;background-color: #ff6000;border-radius: 17px;}*/
+  .sports_list{ width: 1020px; margin: 40px auto;}
+  .sports_list .sports_list_content{ margin:0px; padding: 0px;}
+  .sports_list .sports_list_content li{ list-style: none; padding:0px; width: 48%; height: 374px; display: inline-table; margin: 40px 1% 30px; position: relative; text-align: center; border: 2px solid #313131; float: left;}
+  .sports_list .sports_list_content li:hover{border-color: #ff6000;box-shadow: 0px 0px 30px rgba(0,0,0,0.6);-webkit-transition: border-color 300ms ease-in;-moz-transition: border-color 300ms ease-in;-ms-transition: border-color 300ms ease-in;transition: border-color 300ms ease-in; }
+  .sports_list .sports_list_content li .bg {width: 485px; height: 317px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: -40px -370px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 500ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;}
+  .sports_list .sports_list_content li:hover .bg{background-position-x: 0px;}
+  .sports_list .sports_list_content li .img {width: 485px;height: 370px;position: absolute;bottom: 0px;left: 0px;background-repeat: no-repeat;background-position: 40px 0px;-webkit-transition: background-position 200ms ease-out;-moz-transition: background-position 200ms ease-out;-ms-transition: background-position 500ms ease-out;transition: background-position 500ms ease-out;}
+  .sports_list .sports_list_content li:hover .img{background-position-x: -20px;}
+  .sports_list .sports_list_content li .title{height: 108px; position: absolute;bottom: 0;left: 0;z-index: 3;width: 100%;padding: 15px;text-align: center;background-color: rgba(0,0,0,0.8);}
+  .sports_list .sports_list_content li .title_img{margin-top: 5px;width: 150px;height: 50px;background-size: 100%;display: inline-block;float: left;}
+  .sports_list .sports_list_content li.sport_ug .title_img{background-image: url(../../static/css/img/logo_ug.png); width: 208px; height: 76px;}
+  .sports_list .sports_list_content li.sport_bb .title_img{background-image: url(../../static/css/img/logo_bb.png); width:126px; height: 68px;}
+  .sports_list .sports_list_content li h2{font-size: 18px;margin-top: 23px; display: inline-block;margin-left: 10px; color: #fff;font-weight: normal;vertical-align: bottom; float: left;}
+  .sports_list .sports_list_content li .btn_enter{position: absolute;bottom: 13px;z-index: 4;left: 50%;}
+  .sports_list .sports_list_content li .gameopen_parent{margin-left: 100px; display: inline-block;margin-top: 10px;color: #fff;line-height: 34px;height: 34px;padding: 0px 30px;background-color: #ff6000;border-radius: 17px;}
   .pagebox .pagemenu_slider {
 
     text-align: center;
